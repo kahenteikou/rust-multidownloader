df43fd7f32fa4488f17ee423c32912df5a7ab2be
kokkiemouse <kokkiemouse@gmail.com>
2020-08-22 08:23:10 +0900

    added multi_thread
    Signed-off-by: kokkiemouse <kokkiemouse@gmail.com>

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,12 @@
 #[cfg(test)]
 mod rust_multidownloader {
     use curl::easy::Easy;
+    use std::fs::File;
+    use std::thread;
+    use threadpool::ThreadPool;
+    use std::io::{self, Read, Write, BufReader,BufWriter};
+    use std::sync::{Arc, Barrier};
+    use regex::Regex;
     #[test]
     fn it_works() {
         assert_eq!(2 + 2, 4);
@@ -10,16 +16,35 @@ mod rust_multidownloader {
         "https://cdn.kernel.org/pub/linux/kernel/v5.x/linux-5.4.59.tar.xz".to_string(),
         "https://cdn.kernel.org/pub/linux/kernel/v4.x/linux-4.19.141.tar.xz".to_string()
         ];
-        download(&download_lskun);
+        download(&download_lskun,8);
     }
     fn download_child(urlkun:&String){
+        let url_re = Regex::new(r".+/(.+?)([\?#;].*)?$").unwrap();
+        let urlkun_clone=urlkun.clone();
+        let capskun=url_re.captures(&urlkun_clone).unwrap();
+        let mut writer = BufWriter::new(File::create(&capskun[1]).unwrap());
         let mut easy = Easy::new();
-        easy.url(urlkun).unwrap();
+        easy.url(&urlkun.clone()).unwrap();
+        let mut transfer = easy.transfer();
+        println!("{}",urlkun.clone());
+        transfer.write_function(|data| {
+            writer.write_all(&data);
+            Ok(data.len())
+        }).unwrap();
+        transfer.perform().unwrap();
     }
-    pub fn download(url_list:&[String]){
+    pub fn download(url_list:&[String],max_threads:usize){
         let mut listkun=url_list.clone();
+        let pool = ThreadPool::new(max_threads);
+        let barrier = Arc::new(Barrier::new(url_list.len() + 1));
         for(dl_url) in listkun{
-            println!("{}",&dl_url);
+            let barrier = barrier.clone();
+            let dl_url2=dl_url.clone();
+            pool.execute(move || {
+                download_child(&dl_url2);
+                barrier.wait();
+            });
         }
+        barrier.wait();
     }
 }
