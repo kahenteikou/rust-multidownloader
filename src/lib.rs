2cb77f5d3883d98c3672f2adcfdc6d851450ba9e
kokkiemouse <kokkiemouse@gmail.com>
2020-08-21 21:06:26 +0900

    added ls
    Signed-off-by: kokkiemouse <kokkiemouse@gmail.com>

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,19 @@
 #[cfg(test)]
-mod tests {
+mod rust_multidownloader {
     #[test]
     fn it_works() {
         assert_eq!(2 + 2, 4);
+        let download_lskun=[    "https://cdn.kernel.org/pub/linux/kernel/v5.x/linux-5.8.2.tar.xz".to_string(),
+        "https://cdn.kernel.org/pub/linux/kernel/v5.x/linux-5.7.16.tar.xz".to_string(),
+        "https://cdn.kernel.org/pub/linux/kernel/v5.x/linux-5.4.59.tar.xz".to_string(),
+        "https://cdn.kernel.org/pub/linux/kernel/v4.x/linux-4.19.141.tar.xz".to_string()
+        ];
+        download(&download_lskun);
+    }
+    pub fn download(url_list:&[String]){
+        let mut listkun=url_list.clone();
+        for(dl_url) in listkun{
+            println!("{}",&dl_url);
+        }
     }
 }
